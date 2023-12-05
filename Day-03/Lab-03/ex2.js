54f13fb857858b4e739b27de9fd5f30bdaf72e69
asaiem <eng.abdulrahman.alsaiem@gmail.com>
2023-12-05 15:18:39 +0200

    add username & password validations 01

--- Day-03/Lab-03/ex2.js
+++ Day-03/Lab-03/ex2.js
@@ -4,11 +4,9 @@ let password = prompt('Enter your password')
 function auth(x,y){
     if(x !== 'admin' && y!=='421$$'){
         return alert('wrong user name & password')
-    }
-    if(x !== 'admin'){        
+    } else if(x !== 'admin'){        
         return alert('wrong username')
-    } 
-    if(y!=='421$$'){
+    } else if(y!=='421$$'){
         return alert('wrong password')
     }
     alert('Welconme Succes Login !')
